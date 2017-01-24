4630beae7084cff0ad8e4b1b03d34207f8594812
Rubikoid <Rubikoid@users.noreply.github.com>
2017-01-24 10:45:30 +0300

    Распихиваем шарики, vector2, упоротая физика.
    http://www.gamedev.ru/code/articles/PositionBasedPhysics

--- Balls.h
+++ Balls.h
@@ -4,71 +4,141 @@
 #include <vector>
 #include <string>
 #include "grap.h"
+#include <stdio.h>
 
 #define TIME		GetTickCount()/100
 #define KEY			GetAsyncKeyState
 #define uint 		unsigned int //govnokode for compare with vector.size()
 
 using namespace std;
+class Vector2 {
+    public:
+    float x,y;
+    Vector2(void) { }
+
+    Vector2(float X, float Y) {
+        this->x = X;
+        this->y = Y;
+    }
+
+    // Returns the length of the vector
+    float Length() {
+        return sqrt(x * x + y * y);
+    }
+
+    float sqrLength() {
+        return x * x + y * y;
+    }
+
+    // Normalizes the vector
+    Vector2 Normalize() {
+        Vector2 vector;
+        float length = this->Length();
+
+        if(length != 0) {
+            vector.x = x/length;
+            vector.y = y/length;
+        }
+
+        return vector;
+    }
+
+    Vector2 negative() {
+        return Vector2(x*-1,y*-1);
+    }
+};
+
+Vector2 operator+(const Vector2 &v1, const Vector2 &v2) {
+    return Vector2(v1.x + v2.x, v1.y + v2.y);
+}
+
+Vector2 operator-(const Vector2 &v1, const Vector2 &v2) {
+    return Vector2(v1.x - v2.x, v1.y - v2.y);
+}
+
+Vector2 operator*(const Vector2 &v1, const float &v2) {
+    Vector2 result(v1.x*v2,v1.y*v2);
+    return result;
+}
 
 class base {
     public:
-    double x, y;
-    double vx, vy;
+    Vector2 pos, speed;
 
-    base(double startX=0.0, double startY=0.0, double startVX=0.0, double startVY=0.0) {
-        x = startX;
-        y = startY;
-        vx = startVX;
-        vy = startVY;
+    base(Vector2 startPos, Vector2 startSpeed) {
+        pos = startPos;
+        speed = startSpeed;
     }
 
+    base() { }
+
     void update() {
-        if(x+vx>800 || x+vx<0) {
-            x+=vx;
-            vx*=-1;
+        if(pos.x+speed.x>800 || pos.x+speed.x<0) {
+            pos.x+=speed.x;
+            speed.x*=-1;
         }
-        else x+=vx;
-        if(y+vy>500 || y+vy<0) {
-            y+=vy;
-            vy*=-1;
+        else pos.x+=speed.x;
+        if(pos.y+pos.y>500 || pos.y+pos.y<0) {
+            pos.y+=speed.y;
+            speed.y*=-1;
         }
-        else y+=vy;
+        else pos.y+=speed.y;
     }
 
     void drow() {
-        txSetPixel(x, y, RGB(255,0,0));
-        //txCircle(test.x, test.y, test.radi);
+        txSetPixel(pos.x, pos.y, RGB(255,0,0));
     }
 };
 
 class ball : public base {
     public:
-    double radi;
+    float radi;
     COLORREF color;
-    ball(double startX=0.0, double startY=0.0, double startVX=0.0, double startVY=0.0, double r=0.0, COLORREF colr = RGB(255,255,255)) : base(startX, startY, startVX, startVY) {
+
+    int id;
+    ball(Vector2 startPos, Vector2 startSpeed, double r=0.0, COLORREF colr = RGB(255,255,255), int ID=0) : base(startPos, startSpeed) {
         radi = r;
         color = colr;
+        id = ID;
     }
 
+    ball() : base() {}
+
     void update() {
-        if(800.0-(x+radi) < 0 || (x-radi) < 0) {
-            vx*=-1;
-            x+=vx;
+        if(800.0-(pos.x+speed.x+radi) < 0 || (pos.x+speed.x-radi) < 0) {
+            speed.x*=-1;
         }
-        else x+=vx;
-        if(500.0-(y+radi) < 0 || (y-radi) <=0) {
-            vy*=-1;
-            y+=vy;
+        if(500.0-(pos.y+speed.y+radi) < 0 || (pos.y+speed.y-radi) <=0) {
+            speed.y*=-1;
         }
-        else y+=vy;
+        pos.x+=speed.x;
+        pos.y+=speed.y;
+    }
+
+    void ballCheck(ball &bl)
+    {
+        Vector2 p12 = pos - bl.pos;
+        if(p12.sqrLength() < (radi + bl.radi)*(radi + bl.radi))
+        {
+            Vector2 tmp = bl.speed;
+            bl.speed = speed;
+            speed = tmp;
+            printf("%d;%d\n",id,bl.id);
+            Vector2 p21 = bl.pos - pos, reflDest = p21.Normalize();
+            float reflDepth = radi + bl.radi - p21.Length();
+            pos = pos + reflDest.negative() * reflDepth;
+            bl.pos = bl.pos + reflDest * reflDepth;
+        }
     }
 
     void drow() {
         txSetFillColour(color);
         txSetColor(color, 1);
-        txCircle(x, y, radi);
+        txCircle(pos.x, pos.y, radi);
+        char a[4];
+        sprintf(a,"%d",id);
         resetCol();
+        textOut(pos.x,pos.y,a);
     }
 };
 
--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,10 @@ int main() {
     //vars
     long Time = 0, T0 = 0;
     vector<ball> items = vector<ball>(0);
-    items.push_back(ball(300,100,4 ,2, 30, RGB(255,0,0)));
-    items.push_back(ball(100,40 ,6,4, 20, RGB(100,200,0)));
-    items.push_back(ball(400,300,2, 5, 25, RGB(50,150,100)));
-    items.push_back(ball(300,150,8, 1, 15, RGB(150,10,200)));
+    items.push_back(ball(Vector2(300,100), Vector2(4, 2), 30, RGB(255,0,0),0));
+    items.push_back(ball(Vector2(100, 40), Vector2(6, 4), 20, RGB(100,200,0),1));
+    items.push_back(ball(Vector2(400,300), Vector2(2, 5), 25, RGB(50,150,100),2));
+    items.push_back(ball(Vector2(300,150), Vector2(8, 1), 15, RGB(150,10,200),3));
     //vars
     init(800,500);
     Time = TIME;
@@ -17,9 +17,14 @@ int main() {
     while(!KEY(VK_ESCAPE)) {
         resetCol();
         clear();
-        for(int i=0;i<items.size();i++) {
+        for(int i =0;i<items.size();i++) {
             items[i].drow();
             items[i].update();
+        }
+        for(int i=0; i<items.size();i++) {
+            for(int j=0; j<items.size();j++) {
+                if(i!=j) items[i].ballCheck(items[j]);
+            }
         }
         gsleep(10);
     }
