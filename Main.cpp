c14884744a5de9d40f21ddcca3db882ca1678fc7
Rubikoid <Rubikoid@users.noreply.github.com>
2017-02-14 10:42:04 +0300

    dev

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include "Balls.h"
+#include "grap.h"
 
 #define WIDTH 1300
 #define HEIGHT 700
@@ -7,31 +8,25 @@ int iRand(int min, int max) {
     return min + rand() % (max-min+1);
 }
 
-int main(int argc, char **argv) {
+int main() {
     //vars
     long Time = 0, T0 = 0;
     vector<ball> items = vector<ball>(0);
     init(WIDTH, HEIGHT);
-    /*Bounds box = Bounds(40,HEIGHT-40,WIDTH-40,40);
-    for(int i=0;i<100;i++) {
-        int radi = 5;
-        int startX = iRand(((int)box.ldPos.x)+radi*2,((int)box.rpPos.x)-radi*2), startY = iRand(((int)box.rpPos.y)+radi*2,((int)box.ldPos.y)-radi*2);
-        int speedX = iRand(-10,10), speedY = iRand(-10,10);
-        ball next = ball(Vector2((float)startX,(float)startY),     //start pos
-                         Vector2((float)speedX,(float)speedY),     //start speed
-                         radi,          //mass/radi
-                         RGB(iRand(40,240),iRand(40,240),iRand(40,240)),  //color
-                         (double)i,             //num
-                         box            //box
-                         );
-        //printf("%d;%d;%f;%f;max=%d\n",startX,startY,next.pos.x,next.pos.y,((int)box.ldPos.y)-radi*2);
-        items.push_back(next);
-    }
-    ball next = ball(Vector2(400,200),     //start pos
+    Bounds box = Bounds(40,HEIGHT-40,WIDTH-40,40);
+    ball next = ball(Vector2(400,400),     //start pos
+                     Vector2(2,0),     //start speed
+                     20,          //mass/radi
+                     RGB(255,0,255),  //color
+                     1,             //num
+                     box            //box
+                     );
+    items.push_back(next);
+    next = ball(Vector2(450,425),     //start pos
                      Vector2(0,0),     //start speed
-                     6,          //mass/radi
-                     RGB(255,255,255),  //color
-                     150,             //num
+                     20,          //mass/radi
+                     RGB(255,0,255),  //color
+                     2,             //num
                      box            //box
                      );
     items.push_back(next);
@@ -51,26 +46,12 @@ int main(int argc, char **argv) {
         for(int i=0; i<items.size();i++) {
             for(int j=0; j<items.size();j++) {
                 if(i!=j) {
-                    if(items[j].isExist && items[i].isExist) {
-                        int k = items[i].ballCheck(items[j]);
-                        if(k == 1) {
-                            items[j].isExist = false;
-                            items[i].mass += items[j].mass;
-                            items[i].speed += items[j].speed;
-                            items[i].radiReCalc();
-                        }
-                        if(k == 2) {
-                            items[i].isExist = false;
-                            items[j].mass += items[i].mass;
-                            items[j].speed += items[i].speed;
-                            items[j].radiReCalc();
-                        }
-                    }
+                    int k = items[i].ballCheck(items[j]);
                 }
             }
         }
-        gsleep(10);
+        gsleep(100);
     }
-    end();*/
+    end();
     return 0;
 }
